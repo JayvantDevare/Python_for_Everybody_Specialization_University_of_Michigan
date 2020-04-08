dfe8dd1e472d40d0a151dcba74db5735c4067556
Jayvant Devare <jayvant.devare@gmail.com>
2020-04-08 17:07:31 +0530

    PY4E : all assingments

---
+++ PY4E_Getting_Started_With_Python/Assignment 2.2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      jayvant
+#
+# Created:     08/04/2020
+# Copyright:   (c) jayvant 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+# Assignment 2.2
+# Write a program that uses input to prompt a user for their name and then welcomes them.
+# Note that input will pop up a dialog box. Enter Sarah in the pop-up box
+# when you are prompted so your output will match the desired output.
+
+name = input("Enter your name")
+print("Hello" , name)
